2d45736e25951357b46501c141e757e95e177366
versox <cass.carson@gmail.com>
2020-06-11 15:24:51 -0400

    'chipshare'

---
+++ chipshare/precache-manifest.f1774191c2421a42df96693b7481734a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bec18a5f71a4986b25c1",
+    "url": "/chipshare/static/js/runtime~main.bec18a5f.js"
+  },
+  {
+    "revision": "858094b772b86a1b8a0f",
+    "url": "/chipshare/static/js/main.858094b7.chunk.js"
+  },
+  {
+    "revision": "fabe298bd6abf50277d9",
+    "url": "/chipshare/static/js/1.fabe298b.chunk.js"
+  },
+  {
+    "revision": "858094b772b86a1b8a0f",
+    "url": "/chipshare/static/css/main.9fdc2f47.chunk.css"
+  },
+  {
+    "revision": "b25ed659d2c6b96de4ee2a88dd1f58d8",
+    "url": "/chipshare/index.html"
+  }
+];
--- chipshare/service-worker.js
+++ chipshare/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b96fe7e39bc41139f576c2f223d47262.js"
+  "/chipshare/precache-manifest.f1774191c2421a42df96693b7481734a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/chipshare/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
