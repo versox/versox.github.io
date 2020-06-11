7e8a3bcbf92bb6cbfaca7c50a3e6487908fd5f55
versox <cass.carson@gmail.com>
2020-06-11 15:02:14 -0400

    'chipshare'

---
+++ chipshare/precache-manifest.a5e13984358aefc8ba810a05bf09acae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "0e568c1a562389422485",
+    "url": "./static/js/main.0e568c1a.chunk.js"
+  },
+  {
+    "revision": "fabe298bd6abf50277d9",
+    "url": "./static/js/1.fabe298b.chunk.js"
+  },
+  {
+    "revision": "0e568c1a562389422485",
+    "url": "./static/css/main.9fdc2f47.chunk.css"
+  },
+  {
+    "revision": "e2ae18abc7412cd3b558bf6f570ff92f",
+    "url": "./index.html"
+  }
+];
